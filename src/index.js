d5946093bf114ddf079cdfdfcb8ddda9b1962aa5
htbkoo <h1017760@connect.hku.hk>
2017-12-20 19:04:28 +0800

    fixed flow type error (reference: https://github.com/verekia/js-stack-from-scratch/issues/249) Pomodoro Clock (with React + Redux)

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const ROOT_CONTAINER_SELECTOR = 'root';
+const rootEl = document.getElementById(ROOT_CONTAINER_SELECTOR);
+if (!(rootEl instanceof Element)) {
+    throw new Error('invalid type');
+}
+ReactDOM.render(<App/>, rootEl);
 registerServiceWorker();
