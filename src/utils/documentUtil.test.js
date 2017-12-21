02495eb8de5c12f75ab1f4e03a50242633bec806
htbkoo <h1017760@connect.hku.hk>
2017-12-21 08:25:50 +0800

    slightly refactored documentUtil.test.js Pomodoro Clock (with React + Redux)

--- src/utils/documentUtil.test.js
+++ src/utils/documentUtil.test.js
@@ -7,11 +7,16 @@ import documentUtil from "./documentUtil";
 describe("documentUtil", function () {
     it("should get element if the type is correct", sinonTest(function () {
         const id = "anId", mockElement = sinon.createStubInstance(Element);
-        const document = sinon.createStubInstance(Document);
-        document.getElementById = this.stub().withArgs(id).returns(mockElement);
+        const document = createMockDocument.call(this, id, mockElement);
 
         let element = documentUtil.getElementOrThrow({id, document});
 
         chai.expect(element).to.equal(mockElement, "The experted element is not returned");
     }));
+
+    function createMockDocument(id, mockElement) {
+        const document = sinon.createStubInstance(Document);
+        document.getElementById = this.stub().withArgs(id).returns(mockElement);
+        return document;
+    }
 });
