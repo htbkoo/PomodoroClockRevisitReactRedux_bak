dd88cc21cb189691b5dade56cd58ea8781e200d4
htbkoo <h1017760@connect.hku.hk>
2017-12-21 09:06:45 +0800

    added shouldThrow test case in documentUtil.js Pomodoro Clock (with React + Redux)

--- src/utils/documentUtil.js
+++ src/utils/documentUtil.js
@@ -1,7 +1,11 @@
 // @flow
 
 export default {
-    getElementOrThrow({id, document}: { id: string, document: Document }) {
-        return document.getElementById(id);
+    getElementOrThrow({id, document}: { id: string, document: Document }): Element {
+        const element = document.getElementById(id);
+        if (!(element instanceof Element)) {
+            throw new TypeError(`Invalid type - document.getElementById(${id}) does not return a proper Element`);
+        }
+        return element;
     }
 }
--- src/utils/documentUtil.test.js
+++ src/utils/documentUtil.test.js
@@ -14,6 +14,16 @@ describe("documentUtil", function () {
         chai.expect(element).to.equal(mockElement, "The experted element is not returned");
     }));
 
+    it("should throw TypeError if the element returned is not an Element", sinonTest(function () {
+        const id = "anId", notAnElement = "";
+        const document = sinon.createStubInstance(Document);
+        document.getElementById = sinon.stub().withArgs(id).returns(notAnElement);
+
+        let actionThatShouldThrow = documentUtil.getElementOrThrow.bind(documentUtil, {id, document});
+
+        chai.expect(actionThatShouldThrow).to.throw(TypeError);
+    }));
+
     function createMockDocument(id, mockElement) {
         const document = sinon.createStubInstance(Document);
         document.getElementById = sinon.stub().withArgs(id).returns(mockElement);
