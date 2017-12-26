8a567e1f974fea78de01f57040f641b7e666c083
htbkoo <h1017760@connect.hku.hk>
2017-12-26 16:19:11 +0800

    migrated to use enzyme.mount in App.mount.spec.js Pomodoro Clock (with React + Redux)

--- src/App.mount.spec.js
+++ src/App.mount.spec.js
@@ -1,10 +1,11 @@
+// @flow
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {mount} from "enzyme";
+
 import App from './App';
 
 describe('App - mount test', function () {
     it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App/>, div);
+        mount(<App/>);
     });
 });
