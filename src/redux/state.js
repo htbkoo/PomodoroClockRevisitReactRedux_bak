e06dd3f08860cccc9aa7ad8c3fa1740e8dbb8e65
htbkoo <h1017760@connect.hku.hk>
2017-12-26 11:08:50 +0800

    updated state.js to read-only Pomodoro Clock (with React + Redux)

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,19 +1,19 @@
 // @flow
 
 export type Session = {
-    time: number,
-    clockId: number
+    +time: number,
+    +clockId: number
 };
 
 export type Clock = {
-    id: number,
-    duration: number,
-    name: string,
-    colour: string
+    +id: number,
+    +duration: number,
+    +name: string,
+    +colour: string
 };
 
 export type State = {
-    isCounting: boolean,
-    session: Session,
-    clocks: Array<Clock>
+    +isCounting: boolean,
+    +session: Session,
+    +clocks: Array<Clock>
 };
