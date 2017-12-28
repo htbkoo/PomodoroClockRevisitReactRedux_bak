a4cbacb0fd9b718c17d8c3c2a049e0defa9f01f3
htbkoo <h1017760@connect.hku.hk>
2017-12-28 08:09:09 +0800

    re-organized Clock.test.js Pomodoro Clock (with React + Redux)

--- src/components/Clock.test.js
+++ src/components/Clock.test.js
@@ -6,28 +6,30 @@ import {shallow} from "enzyme";
 import Clock from "./Clock";
 
 describe('Clock', function () {
-    [
-        {time: 0, expectedText: "00m 00s 000"},
-        {time: 99, expectedText: "00m 00s 099"},
-        {time: 100, expectedText: "00m 00s 100"},
-        {time: 999, expectedText: "00m 00s 999"},
-        {time: 1000, expectedText: "00m 01s 000"},
-        {time: 1001, expectedText: "00m 01s 001"},
-        {time: 1999, expectedText: "00m 01s 999"},
-        {time: 2000, expectedText: "00m 02s 000"},
-        {time: 10999, expectedText: "00m 10s 999"},
-        {time: 59999, expectedText: "00m 59s 999"},
-        {time: 60000, expectedText: "01m 00s 000"},
-        {time: 60001, expectedText: "01m 00s 001"},
-        {time: 61001, expectedText: "01m 01s 001"},
-    ].forEach(({time, expectedText}) =>
-        it(`should render props.time=${time} as ${expectedText}`, function () {
-            //    given
-            //    when
-            let clock = shallow(<Clock time={time}/>);
+    describe("ClockComponent", function () {
+        [
+            {time: 0, expectedText: "00m 00s 000"},
+            {time: 99, expectedText: "00m 00s 099"},
+            {time: 100, expectedText: "00m 00s 100"},
+            {time: 999, expectedText: "00m 00s 999"},
+            {time: 1000, expectedText: "00m 01s 000"},
+            {time: 1001, expectedText: "00m 01s 001"},
+            {time: 1999, expectedText: "00m 01s 999"},
+            {time: 2000, expectedText: "00m 02s 000"},
+            {time: 10999, expectedText: "00m 10s 999"},
+            {time: 59999, expectedText: "00m 59s 999"},
+            {time: 60000, expectedText: "01m 00s 000"},
+            {time: 60001, expectedText: "01m 00s 001"},
+            {time: 61001, expectedText: "01m 01s 001"},
+        ].forEach(({time, expectedText}) =>
+            it(`should render props.time=${time} as ${expectedText}`, function () {
+                //    given
+                //    when
+                let clock = shallow(<Clock time={time}/>);
 
-            //    then
-            expect(clock).toHaveText(expectedText);
-        })
-    );
+                //    then
+                expect(clock).toHaveText(expectedText);
+            })
+        );
+    });
 });
