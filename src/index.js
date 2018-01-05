ba46dfca2a8a8bb5cebdf34f78a3dcc681cb2b82
htbkoo <h1017760@connect.hku.hk>
2018-01-05 23:38:16 +0800

    slightly reformatted index.js Pomodoro Clock (with React + Redux)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 // @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './App';
+
 import documentUtil from "./utils/documentUtil";
 
+import './index.css';
+
 const ROOT_CONTAINER_SELECTOR = 'root';
-const rootEl = documentUtil.getElementOrThrow({id: ROOT_CONTAINER_SELECTOR, document});
+
+let rootEl = documentUtil.getElementOrThrow({id: ROOT_CONTAINER_SELECTOR, document});
 ReactDOM.render(<App/>, rootEl);
+
 registerServiceWorker();
