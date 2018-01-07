26836702559438982495f28e5a1af72a318f8cb9
htbkoo <h1017760@connect.hku.hk>
2018-01-07 23:00:40 +0800

    created TimeTicker.js with mapStateToProps Pomodoro Clock (with React + Redux)

--- src/components/TimeTicker.js
+++ src/components/TimeTicker.js
@@ -1,25 +1,8 @@
 // @flow
-import React from "react";
-import moment from "moment";
-import {connect} from "react-redux";
-import type {Session, State} from "../redux/state";
+import type {State} from "../redux/state";
 
 type Props = {
-    time: number
+    +isCounting: boolean
 }
 
-export const mapStateToProps = (state: State): Session => state.session;
-export const TimeTickerComponent = (props: Props): React$Element<any> => (
-    <div id="clock-time" className="TimeTicker">{formatTime(props.time)}</div>
-);
-
-// Untested
-export default connect(mapStateToProps)(TimeTickerComponent);
-
-TimeTickerComponent.defaultProps = {
-    displayName: 'TimeTicker'
-};
-
-function formatTime(time: number): string {
-    return moment(time).format(`mm[m] ss[s] SSS`);
-}
+export const mapStateToProps = (state: State): Props => ({isCounting: state.isCounting});
