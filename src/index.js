647c565594745032d9a53b5e65c3754bd2a02fb0
htbkoo <h1017760@connect.hku.hk>
2018-01-07 17:52:23 +0800

    updated index.js to incorporated the usage of redux store Pomodoro Clock (with React + Redux)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-import App from './App';
-
+import AppWithStore from "./components/AppWithStore";
 import documentUtil from "./utils/documentUtil";
 
 import './index.css';
@@ -12,6 +11,6 @@ import './index.css';
 const ROOT_CONTAINER_SELECTOR = 'root';
 
 let rootEl = documentUtil.getElementOrThrow({id: ROOT_CONTAINER_SELECTOR, document});
-ReactDOM.render(<App/>, rootEl);
+ReactDOM.render(<AppWithStore/>, rootEl);
 
 registerServiceWorker();
