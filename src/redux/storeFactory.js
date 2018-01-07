07f0dd5302df49c4c6d7d927cd5432a6c57bb2ac
htbkoo <h1017760@connect.hku.hk>
2018-01-07 17:03:29 +0800

    added storeFactory.AppWithStore Pomodoro Clock (with React + Redux)

--- src/redux/storeFactory.js
+++ src/redux/storeFactory.js
@@ -1,10 +1,22 @@
 // @flow
+import React from "react";
 import type {Reducer, Store} from "redux";
 import {createStore} from "redux";
+import {Provider} from "react-redux";
+
 import type {State} from "./state";
 import type {Action} from "./actions";
 import defaultReducers from "./reducers";
+import App from "../App";
 
 export function newStore(reducers: Reducer<State, Action> = defaultReducers): Store<State, Action> {
     return createStore(reducers);
 }
+
+const AppWithStore = (prop: { store: Store<State, Action> }) => (
+    <Provider store={prop.store}>
+        <App/>
+    </Provider>
+);
+
+export {AppWithStore};
--- src/redux/storeFactory.test.js
+++ src/redux/storeFactory.test.js
@@ -1,9 +1,15 @@
 // @flow
 import {sinonTest} from "../testUtils/sinonWithTest";
+import React from "react";
 import * as redux from "redux";
 
 import * as storeFactory from "./storeFactory";
+import {AppWithStore} from "./storeFactory";
 import reducers from "./reducers";
+import {shallow} from "enzyme";
+import {Provider} from "react-redux";
+import App from "../App";
+import {newStore} from "./storeFactory";
 
 describe('storeFactory', function () {
     describe("newStore", function () {
@@ -29,6 +35,29 @@ describe('storeFactory', function () {
 
             //    then
             expect(store).toEqual(mockStore);
+        }));
+    });
+
+    /*
+            <Provider store={store}>
+                <App/>
+            </Provider>
+    * */
+
+    describe("AppWithStore", function () {
+        it("should create <AppWithStore/>", sinonTest(function () {
+            //    given
+            const mockStore = Symbol("mockStore");
+            this.stub(redux, "createStore").withArgs(reducers).returns(mockStore);
+
+            //    when
+            let appWithStore = shallow(<AppWithStore store={newStore()}/>);
+
+            //    then
+            let provider = appWithStore.find(Provider);
+            expect(provider).toHaveLength(1);
+            expect(provider.prop("store")).toBe(mockStore);
+            expect(provider.find(App)).toHaveLength(1);
         }));
     });
 });
