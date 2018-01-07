8287cd3b80e95be235944093c9e22bf02be84fa8
htbkoo <h1017760@connect.hku.hk>
2018-01-07 16:44:19 +0800

    extracted acceptance tests to App.acceptance.spec.js from App.mount.spec.js Pomodoro Clock (with React + Redux)

---
+++ src/App.acceptance.spec.js
@@ -0,0 +1,42 @@
+// @flow
+import {Provider} from 'react-redux';
+import React from 'react';
+import {mount} from "enzyme";
+
+import App from './App';
+import ButtonsPanel from "./components/ButtonsPanel";
+import {newStore} from "./redux/storeFactory";
+
+describe('App - acceptance test', function () {
+    it('should be able to click start then change state.isCounting to true', function () {
+        // given
+        const store = newStore();
+        const app = mount(
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        );
+        const getButtonsPanel = () => app.find(ButtonsPanel);
+        const getStartButton = () => app.find("#btn_start");
+
+        expect(getButtonsPanel()).toHaveLength(1);
+
+        expect("isCounting" in store.getState()).toEqual(true);
+        expect(store.getState().isCounting).toEqual(false);
+        expect(getStartButton()).toHaveLength(1);
+
+        // when
+        getStartButton().simulate("click");
+
+        // then
+        expect("isCounting" in store.getState()).toEqual(true);
+        expect(store.getState().isCounting).toEqual(true);
+        expect(getStartButton()).toHaveLength(0);
+    });
+
+    xit('should, when state.isCounting=true, count down the time per 100ms', function () {
+    });
+
+    xit('should, when state.isCounting=false, not count down the time', function () {
+    });
+});
--- src/App.mount.spec.js
+++ src/App.mount.spec.js
@@ -6,7 +6,6 @@ import {mount} from "enzyme";
 
 import App from './App';
 import {StateBuilder} from "./redux/state";
-import ButtonsPanel from "./components/ButtonsPanel";
 
 describe('App - mount test', function () {
     const middlewares = [];
@@ -38,39 +37,6 @@ describe('App - mount test', function () {
 
         // then
         expect(clockTime).toIncludeText(expectedTime)
-    });
-
-    it('should be able to click start then change state.isCounting to true', function () {
-        // given
-        const store = getDefaultStore();
-        const app = mount(
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        );
-        const getButtonsPanel = () => app.find(ButtonsPanel);
-        const getStartButton = () => app.find("#btn_start");
-
-        expect(getButtonsPanel()).toHaveLength(1);
-
-        expect("isCounting" in store.getState()).toEqual(true);
-        expect(store.getState().isCounting).toEqual(false);
-        expect(getStartButton()).toHaveLength(1);
-
-        // when
-        getStartButton().simulate("click");
-
-        // then
-        // TODO
-        // expect("isCounting" in store.getState()).toEqual(true);
-        // expect(store.getState().isCounting).toEqual(true);
-        // expect(getStartButton()).toHaveLength(0);
-    });
-
-    xit('should, when state.isCounting=true, count down the time per 100ms', function () {
-    });
-
-    xit('should, when state.isCounting=false, not count down the time', function () {
     });
 
     function getDefaultStore() {
