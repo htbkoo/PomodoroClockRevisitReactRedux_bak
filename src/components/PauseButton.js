473df7b11d9d24f8b05e76b94dabd830387eb625
htbkoo <h1017760@connect.hku.hk>
2018-01-11 19:18:39 +0800

    reverted changes to fix broken tests Pomodoro Clock (with React + Redux)

--- src/components/PauseButton.js
+++ src/components/PauseButton.js
@@ -1,9 +1,21 @@
 // @flow
 import React from "react";
-import {pauseCounting} from "../redux/actions";
+import {connect} from "react-redux";
+
+type Props = {
+}
 
 export const mapDispatchToProps = (dispatch: Function) => ({
     onPauseClick() {
         dispatch(pauseCounting());
     }
 });
+
+export const PauseButtonComponent = (props: Props): React$Element<any> => (
+    <div id="btn_pause">
+
+    </div>
+);
+
+// Untested
+export default connect(null, mapDispatchToProps)(PauseButtonComponent);
