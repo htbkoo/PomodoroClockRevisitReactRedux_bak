b41236839f80887818b3f25698083ba900465be1
htbkoo <h1017760@connect.hku.hk>
2018-01-12 09:00:28 +0800

    refactored objectUtil.test.js Pomodoro Clock (with React + Redux)

--- src/utils/objectUtil.test.js
+++ src/utils/objectUtil.test.js
@@ -4,14 +4,17 @@ import * as objectUtil from "./objectUtil";
 
 describe("objectUtil", function () {
     describe("isDefined", function () {
-        const expected = true, value = "someString";
-        it(`should return ${expected.toString()} for "${value}"`, function () {
-            //    given
-            //    when
-            let actual = objectUtil.isDefined(value);
+        [
+            {expected: true, value: "someString"}
+        ].forEach(({expected, value}) =>
+            it(`should return ${expected.toString()} for "${value}"`, function () {
+                //    given
+                //    when
+                let actual = objectUtil.isDefined(value);
 
-            //    then
-            expect(actual).toEqual(expected);
-        });
+                //    then
+                expect(actual).toEqual(expected);
+            })
+        );
     });
 });
