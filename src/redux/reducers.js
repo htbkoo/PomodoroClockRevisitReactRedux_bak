5fa4c32c8802dea72c55129af768883631369f4c
htbkoo <h1017760@connect.hku.hk>
2018-01-17 19:02:23 +0800

    incorporated the usage of combineReducers in reducers.js Pomodoro Clock (with React + Redux)

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,32 +1,48 @@
 // @flow
-import type {State} from "./state";
+
+import {combineReducers} from "redux";
+
+import type {Clocks, Session} from "./state";
+import {newInitialStateBuilder} from "./state";
 import type {Action} from "./actions";
 import {actionTypes} from "./actions";
 
-export default function reducers(state: State, action: Action): State {
+const defaultInitialState = newInitialStateBuilder().build();
+
+export default combineReducers({
+    interval,
+    session,
+    clocks
+});
+
+function session(state: Session = defaultInitialState.session, action: Action): Session {
     switch (action.type) {
-        case actionTypes.StartCounting:{
+        case actionTypes.StartCounting: {
             let isCounting = true;
-            let session = Object.assign({}, state.session, {isCounting});
-            return Object.assign({}, state, {session});
+            return Object.assign({}, state, {isCounting});
         }
         case actionTypes.PauseCounting: {
             let isCounting = false;
-            let session = Object.assign({}, state.session, {isCounting});
-            return Object.assign({}, state, {session});
+            return Object.assign({}, state, {isCounting});
         }
         case actionTypes.StopCounting: {
-            let time = state.session.originalTime;
-            let isCounting= false;
-            let session = Object.assign({}, state.session, {time, isCounting});
-            return Object.assign({}, state, {session});
+            let time = state.originalTime;
+            let isCounting = false;
+            return Object.assign({}, state, {time, isCounting});
         }
         case actionTypes.TickTime: {
-            let time = state.session.time - action.lapse;
-            let session = Object.assign({}, state.session, {time});
-            return Object.assign({}, state, {session});
+            let time = state.time - action.lapse;
+            return Object.assign({}, state, {time});
         }
         default:
             return state;
     }
+}
+
+function clocks(state: Clocks = defaultInitialState.clocks): Clocks {
+    return state;
+}
+
+function interval(state: number = defaultInitialState.interval): number {
+    return state;
 }
